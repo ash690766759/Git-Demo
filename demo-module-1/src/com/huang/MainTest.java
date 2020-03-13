06b00b26e19818b8cf3a61f87e3c8a390c8b1818
ash690766759 <s690766759@163.com>
2020-03-13 11:08:12 +0800

    test分支提交  hello test

--- demo-module-1/src/com/huang/MainTest.java
+++ demo-module-1/src/com/huang/MainTest.java
@@ -4,6 +4,6 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("test change");
         System.out.println("test change");
-        System.out.println("test merge");
+        System.out.println("test test");
     }
 }
